8d193788a2ce560ccfe6386b6413bab4156d741f
hardboil <hardboil@gmail.com>
2020-10-10 01:46:14 +0900

    Updates

---
+++ precache-manifest.91c703074a669abdf608d9c8b76040ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d5c19283ff3aa30cbb949b4a6cb70f",
+    "url": "/react_habit_tracker/index.html"
+  },
+  {
+    "revision": "9566d6d0cc8696b97502",
+    "url": "/react_habit_tracker/static/css/main.de8df93f.chunk.css"
+  },
+  {
+    "revision": "4dcd2c573989837010d8",
+    "url": "/react_habit_tracker/static/js/2.02e277e7.chunk.js"
+  },
+  {
+    "revision": "746582258d70325461391e08406982a5",
+    "url": "/react_habit_tracker/static/js/2.02e277e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9566d6d0cc8696b97502",
+    "url": "/react_habit_tracker/static/js/main.4807bbc1.chunk.js"
+  },
+  {
+    "revision": "a241dafdb097aafbf5f4",
+    "url": "/react_habit_tracker/static/js/runtime-main.2538eb4d.js"
+  }
+]);
